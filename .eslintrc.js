2872efa63c00b6866068b552767c166a6d56ee76
newblue <greecenew@foxmail.com>
2021-12-09 14:52:08 +0800

    fix: eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/standard", "@vue/prettier"],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/standard",
+    "@vue/prettier",
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
