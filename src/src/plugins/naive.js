a6939cb25c28cb3a7ff4ba69a639d0b162a4a322
newblue <greecenew@foxmail.com>
2021-12-09 14:19:47 +0800

    版本依赖关系修复

---
+++ src/src/plugins/naive.js
@@ -0,0 +1,19 @@
+import {
+  // create naive ui
+  create,
+  // component
+  NButton,
+  NSpace,
+  NGrid,
+  NGridItem,
+} from 'naive-ui'
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
+const naive = create({
+  components: [NButton, NSpace, NGrid, NGridItem],
+})
+
+export default naive
