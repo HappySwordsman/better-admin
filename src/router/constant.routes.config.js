ce06f375ddbe25cb82ff5fb28ad41df7fe445805
newblue <greecenew@foxmail.com>
2022-01-05 14:12:25 +0800

    remove: 废弃组件

--- src/router/constant.routes.config.js
+++ src/router/constant.routes.config.js
@@ -11,14 +11,6 @@ export default [
     component: () => import("@/views/Layout"),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import("@/views/About.vue"),
-  },
-  {
     path: "/:pathMatch(.*)*",
     name: "NotFound",
     component: () => import("@/views/common/NotFound"),
